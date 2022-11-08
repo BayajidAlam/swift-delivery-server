ffbc50944bac13ff403fa477416053f4f9a8ae86
bayajidalam <bayajidalam2001@gmail.com>
2022-11-08 19:59:25 +0600

    specific data loding route added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -10,18 +10,39 @@ app.use(cors())
 app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@myclaster-1.wxhqp81.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-const serviceCollection = client.db('swiftDelivery').collection('services')
-async function run(){
 
-app.get('/services',async(req,res)=>{
-  const query = {}
-  const cursor = serviceCollection.find(query)
-  const result = await cursor.toArray()
-  res.send(result)
-})
+async function run(){
+  try{
+
+    const serviceCollection = client.db('swiftDelivery').collection('services')
+    // get data of all services 
+    app.get('/services',async(req,res)=>{
+      const query = {}
+      const cursor = serviceCollection.find(query)
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+    // get data of 3 services for home 
+    app.get('/servicesOfHome',async(req,res)=>{
+      const query = {}
+      const cursor = serviceCollection.find(query)
+      const result = await cursor.limit(3).toArray()
+      res.send(result)
+    })
+    // get a specific data of service 
+    app.get('/services/:id',async(req,res)=>{
+      const id = req.params.id 
+      const query = {_id: ObjectId(id)}
+      const serivice = await serviceCollection.findOne(query)
+      res.send(serivice) 
+    })
+  }
+  finally{
+    
+  }
 
 }
 run().catch(err=>console.log(err))
