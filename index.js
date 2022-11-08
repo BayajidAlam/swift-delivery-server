27abb259b5a3589b3467e96d8f92068c00ef56c1
bayajidalam <bayajidalam2001@gmail.com>
2022-11-08 11:38:10 +0600

    DB connected

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
 
+// middlewares
+app.use(cors())
+app.use(express.json())
 
-app.get('/',(req,res)=>{
-  res.send("SERVER IS RUNNING")
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@myclaster-1.wxhqp81.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const serviceCollection = client.db('swiftDelivery').collection('services')
+async function run(){
+
+app.get('/services',async(req,res)=>{
+  const query = {}
+  const cursor = serviceCollection.find(query)
+  const result = await cursor.toArray()
+  res.send(result)
 })
 
+}
+run().catch(err=>console.log(err))
 
 app.listen(port,()=>{
   console.log(`COOL SERVER IS RUNNING ON ${port}`)
