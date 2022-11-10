666e9c89df07c7cab204f5debfa70d3709875ec1
bayajidalam <bayajidalam2001@gmail.com>
2022-11-10 12:44:16 +0600

    almost done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
+var jwt = require('jsonwebtoken');
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -14,11 +15,44 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@myc
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+///------------jwt---------------///
+
+function verityJWT(req,res,next){
+    const authHeader = req.headers.authorization 
+
+    if(!authHeader){
+      return res.status(401).send({message : 'unauthorized access'})
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET, function(err,decoded){
+      if(err){
+          return res.status(401).send({message: 'unauthorized access'})
+      }
+      req.decoded = decoded;
+      next()
+    })
+}
+
+///------------jwt---------------///
+
 async function run(){
   try{
 
     const serviceCollection = client.db('swiftDelivery').collection('services')
     const reviewCollection = client.db('swiftDelivery').collection('reviews')
+
+    //--------------------------------------//
+
+      app.post('/jwt', (req,res)=>{
+        const user = req.body
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '6h' })
+        res.send({token})
+      })
+
+
+    //---------------------------------------//
+
+
     //-------------------------------------//
 
     // get data of all services 
@@ -78,17 +112,21 @@ async function run(){
       res.send(reviews)
     })
 
-    // get a user specific user data from database and send to client 
-    app.get('/reviews', async (req,res)=> {
-      let query = {}
-      if(req.query.email){
-        query = {          
+    // get a user specific user data by query from database and send to client 
+    app.get('/reviewsquery', verityJWT, async (req,res)=> {
+      const decoded = req.decoded 
+      if(decoded.email !== req.query.email){
+        res.status(403).send({message: 'unathorized access'})
+      }
+     let query = {};
+     if(req.query.email){
+        query = {
           reviewerEmail : req.query.email
         }
-      }
-      const cursor = reviewCollection.find(query)
-      const reviews = await cursor.toArray()
-      res.send(reviews)
+     }
+     const cursor = reviewCollection.find(query);
+     const result = await cursor.toArray();
+     res.send(result)
     })
 
     // delete specific item 
